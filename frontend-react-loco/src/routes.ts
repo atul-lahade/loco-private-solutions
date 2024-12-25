c5799bdcbd6769a1878c176683af052bef175ead
Atul Lahade <atul.lahade@mendix.com>
2024-12-26 00:14:11 +0530

    removed react project

--- frontend-react-loco/src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
-
-export default [
-    route("/", "App.tsx", [
-        route("name-gen", "routes/name-gen.tsx"),
-    ]),
-
-] satisfies RouteConfig;
