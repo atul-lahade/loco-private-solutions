f37acde0978244ed43c8dd3f812e2d27c9ddb8fc
Atul Lahade <atul.lahade@mendix.com>
2025-02-14 12:43:10 +0530

    Removed MUI Taiwind references

--- frontend-react-loco/tailwind.config.ts
+++ frontend-react-loco/tailwind.config.ts
@@ -1,13 +1,12 @@
 import type { Config } from "tailwindcss";
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+module.exports ={
   content: [
     "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: {  
     extend: {
       fontFamily: {
         'sans': ['Helvetica', 'Arial', 'sans-serif'],
@@ -15,4 +14,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-}) satisfies Config;
+} satisfies Config;
